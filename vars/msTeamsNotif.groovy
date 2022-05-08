c8909252929499bdee621518da44e444ccaf8897
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-09 01:59:47 +0800

    Create msTeamsNotif.groovy

---
+++ vars/msTeamsNotif.groovy
@@ -0,0 +1,31 @@
+def call(String buildStatus, String jobName, String webhookUrl) {
+  def cause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+  echo "userName: ${cause.userName}"
+  def now = "${new Date().format('yyyyMMdd')}"  
+  
+  def emoji
+  
+  if(currentBuild.result == ('FAILURE')){
+    emoji = "❌" } else { emoji = "🚀" }
+
+   sh """
+      curl --request POST \
+      --url ${webhookUrl} \
+      --header 'content-type: application/json' \
+      --data '{
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": "0076D7",
+        "summary": "Unified Notifier",
+        "sections": [{"activityTitle": "Unified Notifier",
+        "facts": [{"name": "task",
+        "value": "${emoji} ${jobName}"},
+        {"name": "status",
+        "value": "${buildStatus}",
+        {"name": "started_by",
+        "value": "${cause.userName}"},
+        {"name": "date",
+        "value": "${now}"}],
+        "markdown": true}]}'
+      """
+}
