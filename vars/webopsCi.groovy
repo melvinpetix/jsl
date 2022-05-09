f24eba25a67400d1852aea059f246cbdf762e4d5
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-09 14:58:44 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -35,9 +35,7 @@ def call(){
            [$class: 'StringParameterDefinition', description: '', name: '', trim: 'true' ]])  
         }
       }
-      j.parameters.name = "${params}"
-      println "${j.parameters.name}"
-      
+
       /*
       if(j.parameters == 'string'){
          params = input(id: 'String1', message: '', parameters: [
