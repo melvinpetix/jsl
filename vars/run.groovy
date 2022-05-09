9c7fbfec36a2379075ecb66d3df88c4b5e9dcc83
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-09 19:11:52 +0800

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,10 +1,10 @@
 import com.webops.util.ArrayMap
 
-def call(args) {
+def call(command) {
     long ts = System.currentTimeMillis()
     String outFile = "out_$ts".toString()
     String errFile = "err_$ts".toString()
-    int status = sh(script: "${args.script} 1> $outFile 2> $errFile", returnStatus: true)
+    int status = sh(script: command 1> $outFile 2> $errFile", returnStatus: true)
     String out = readFile(file: outFile)
     String err = readFile(file: errFile)
     sh script: "rm $outFile $errFile", returnStatus: true
