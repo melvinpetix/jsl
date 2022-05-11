5e7309cd4e27a9a40f35cb53eb52069a4c97d270
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-12 02:04:21 +0800

    Create dockerRun.groovy

---
+++ vars/dockerRun.groovy
@@ -0,0 +1,39 @@
+def call(image, command, environment = [], volumes = [], failErrorMessage, cleanupDockerCommand, removeAfterRun) {
+
+  def volumesSwitch = '', environmentSwitch = '', failSh = ''
+
+  //generate volumes switch
+  if (volumes != null && volumes.size() > 0) {
+    def keys = volumes.keySet();
+    for (int i = 0; i < volumes.size(); i++) {
+      volumesSwitch = volumesSwitch + "-v ${keys[i]}:${volumes[keys[i]]} "
+    }
+  }
+
+  //generate volumes switch
+  if (environment != null && environment.size() > 0) {
+    def keys = environment.keySet();
+    for (int i = 0; i < environment.size(); i++) {
+      environmentSwitch = environmentSwitch + "-e ${keys[i]}=${environment[keys[i]]} "
+    }
+  }
+
+  if (failErrorMessage == null) failErrorMessage = "Docker image ${image} failed to run with cmd ${command}"
+
+  if (cleanupDockerCommand != null) {
+    failSh = "\tdocker run --rm $volumesSwitch $environmentSwitch $image $cleanupDockerCommand"
+  }
+  failSh = failSh + "\n\techo \"$failErrorMessage\"\n\texit 2"
+
+  def cmd =
+          """
+          #/bin/bash -xx
+          docker run ${removeAfterRun ? '--rm' : ''} $volumesSwitch $environmentSwitch $image $command
+          if [ \$? -ne 0 ]; then
+              $failSh
+          fi
+          """
+
+  sh cmd
+
+}
