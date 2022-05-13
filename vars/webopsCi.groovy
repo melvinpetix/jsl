f8f10004f29f88f290989e36a23d930a8d1c94a5
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-14 06:06:18 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -1,16 +1,20 @@
 @Library('github.com/melvinpetix/jsl@main')_
 import com.webops.*;
+def common = new com.webops.Common()
 
 def call(){
   node("${env.jenkins_agent}"){  
-    def common = new com.webops.Common()
-    def BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" 
-    def cause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
 
-    deleteDir()
+  def BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" 
+  def cause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+  def stringParams
+  def choiceParams
+  def j
 
-    git branch: 'main', url: 'https://oauth:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git'
-    sh 'chmod 600 .config/*'   
+  deleteDir()
+
+  common.gitClone 'gitlab.com/me1824/jsl', 'glpat-GxfR6J-STGecxjDPGz8z', 'main'
+  //common.gitCheckout 'mbiscarra/legacy-task.git', 'prd-private-gitlab', 'script'
 
     stage('define task'){
       def folders = sh(returnStdout: true, script: "ls $workspace/runbook").replaceAll(".yml", "")
@@ -18,65 +22,64 @@ def call(){
       tasklist = readFile("$workspace/task.txt") 
       userInput = input(id: 'tasklist', message: 'task', parameters: [
       [$class: 'ChoiceParameterDefinition', choices: "${tasklist}", description: '', name: 'tasklist']]) 
+
+        /*
+        if(j.project_name == 'MASTER_TEMPLATE'){
+            def snapshot = sh(returnStdout: true, 
+            script: "ssh -F config/config csendrepo01 'ls /data/ENDECA_DATA_REPO_6.5/FULL/MERGE'").trim()
+            writeFile file:'snapshot.txt', text: "${snapshot}"
+            snaplist = readFile("$workspace/snapshot.txt") 
+            snapshot_date = input(id: 'snap', message: 'snapshot', parameters: [
+            [$class: 'ChoiceParameterDefinition', choices: "${snaplist}", description: '', name: '']])  
+            sh "sed -i 's|SNAPSHOT|${snapshot_date}|g' scripts/hotcopy2.sh"
+        }
+        */  
     }
 
     stage("${userInput}"){
-      def j = jobCfg("$workspace/runbook/${userInput}.yml")
+      j = jobCfg("$workspace/runbook/${userInput}.yml")
       list stepsA = j.steps
       list enV = j.environment
-      def params = ''
-       
-      if(j.parameters){
-        if(j.parameters.type == 'string'){
-          params = input(id: 'String1', message: "${j.parameters.name}", parameters: [
-          [$class: 'StringParameterDefinition', description: '', name: '', trim: 'true' ]])  
-        }
-      }
+      list commandA = j.steps.command
+      def serverA = j.steps.server
+      def sshArgs = "ssh -F + -t "
 
+      if(j.parameters.string){
+        stringParams = input(id: 'userInput', message: "${j.parameters.string.name}", parameters: [
+        [$class: 'StringParameterDefinition', defaultValue: '', description: "${j.parameters.string.name}", 
+        name: "${j.parameters.string.name}", trim: true]])
+        println stringParams
+      }
+    
+      if(j.parameters.choice){
+        def choices = j.parameters.choice.choices.replaceAll(',',"\n")
+        choiceParams = input(id: '', message: "${j.parameters.choice.name}", parameters: [
+        [$class: 'ChoiceParameterDefinition', choices: "${choices}", description: '', 
+        name: "${j.parameters.choice.name}"]])  
+        println choiceParams
+      }
+  
       if(j.notification){
         common.sendTeamsNotif("${BUILD_TRIGGER_BY}", j.project_name, j.notification.webhook)
       }
-    
-      if(j.environment){
-        enV.collect{a,b->
-          withEnv(["${a}=${b}"]){
-            stepsA.collect{k,v->
-              stage("${k}"){
-                v.each{command->
-                  sh"""#!/bin/bash +x
-                  export TERM=xterm-256color
-                  export "${j.parameters.name}"="${params}"
-                  ssh -F + ${server} "${command}"
-                  """ 
-                }
+
+      stepsA.name.each{step->
+        stage("${step}"){
+          serverA.each{server->
+            commandA.collect{v->
+              v.each{command->
+sh """#!/bin/bash +x\n
+export "${j.parameters.string.name}"="${stringParams}"\n\
+export "${j.parameters.choice.name}"=${choiceParams}\n\
+${sshArgs} ${server} "export TERM=xterm-256color;
+set -x; ${command}"
+"""
               }
             }
-          }   
+          }
         }
-      } 
-      else{
-        stepsA.collect{k,v->
-          stage("${k}"){
-            v.each{command->
-              if(!j.server || !j.server."${k}" || j.server."${k}" == 'local'){
-                sh"""#!/bin/bash +x
-                export TERM=xterm-256color
-                export snapshot_date="${params}"
-                ${command}
-                """ 
-              } else {
-              def server = j.server."${k}"
-                sh"""#!/bin/bash +x
-                export TERM=xterm-256color
-                export "${j.parameters.name}"="${params}"
-                ssh -F + ${server} "${command}"
-                """ 
-              }                      
-            }
-          }   
-        }
-      }  
+      }
     }
-  }
+  } 
 }
 
