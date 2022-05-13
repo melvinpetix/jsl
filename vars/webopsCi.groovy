1c6bff8bec4f560ed90620f6fb04c1d45e52cf12
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-14 06:52:34 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -1,10 +1,10 @@
 @Library('github.com/melvinpetix/jsl@main')_
 import com.webops.*;
-def common = new com.webops.Common()
+
 
 def call(){
   node("${env.jenkins_agent}"){  
-
+  def common = new com.webops.Common()
   def BUILD_TRIGGER_BY = "${currentBuild.getBuildCauses()[0].shortDescription} / ${currentBuild.getBuildCauses()[0].userId}" 
   def cause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
   def stringParams
