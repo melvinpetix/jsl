ecfa6fa4c1ff326263cc8d9cf25a124b448fc38d
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-16 21:05:54 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -4,22 +4,18 @@ import com.webops.*;
 
 def call(String yamlName) {
     def yaml = readYaml file: yamlName;
-
     def buildNumber = Integer.parseInt(env.BUILD_ID);
-
     // load project's configuration
     ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
-
     // adds the last step of the build.
-    def closure = buildSteps(projectConfig, customImage);
+    def closure = buildSteps(projectConfig)
 
-    // each service is a closure that when called it executes its logic and then calls a closure, the next step.
     projectConfig.services.each {
 
         closure = "${it.service.getVar()}"(projectConfig, it.version, closure);
     }
 
-    // we execute the top level closure so that the cascade starts.
+
     try {
         closure([:]);
     } finally{
