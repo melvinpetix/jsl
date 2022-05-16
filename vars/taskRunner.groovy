4bc72971a2a48b36a0649e133f379284a7349d8e
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-16 14:58:24 +0800

    Update taskRunner.groovy

--- vars/taskRunner.groovy
+++ vars/taskRunner.groovy
@@ -1,18 +1,8 @@
 import com.webops.Parser
 import com.webops.PipelineConfiguration
 
-def call(configFile){
-    gitCheckout(gitlab.com/me1824/jsl, main)
-    def yaml = readYaml file: configFile
+def call(String runbook){
+    Map projectConfig = readYaml(file: "${WORKSPACE}/runbook/" + runbook + ".yml")
+    return projectConfig
 }
-def gitCheckout(String repo, String branch='main') {
-    checkout([$class: 'GitSCM', branches: 
-    [[name: "${main}"]], extensions: [
-    [$class: 'RelativeTargetDirectory',  
-     relativeTargetDir: '.']],            
-    serRemoteConfigs: [[credentialsId: 
-    'prd-private-gitlab', url: 'git@gitlab.usautoparts.io:' + repo]]])
-    sh "set +x; chmod 600 \$(find . -name \"*.key\"||\"*.pub\"||\"id_rsa\")"
-   }  else { checkout scm }
-     
- }
+
