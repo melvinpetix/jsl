1a514a5b38b5cf3da00493f141db905a033b5c59
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-17 20:18:01 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -1,93 +1,44 @@
 @Library('github.com/melvinpetix/jsl@main')_
 import com.webops.*;
+def common = new com.webops.Common() 
 
-
-node("${env.jenkins_agent}"){  
-
-    stage('parameter definition'){
-        
-        common.gitClone 'gitlab.com/me1824/jsl', 
-                        'glpat-GxfR6J-STGecxjDPGz8z', 'main'
+def call(){
     
-        def files = sh(returnStdout: true, 
-            script: "ls $workspace/runbook").replaceAll(".yml", "")
-  
-        def runbook = input(id: 'tasklist', message: 'task', 
-            parameters: [[$class: 'ChoiceParameterDefinition', 
-            choices: "${files}", description: '', name: 'tasklist']]) 
-            
-        pipelineConfig = jobCfg("$workspace/runbook/${runbook}.yml")
+    def builder
+    
+    common.gitClone 'gitlab.com/me1824/jsl', 'glpat-GxfR6J-STGecxjDPGz8z', 'main'
 
-        if(pipelineConfig.parameters){
-            switch(pipelineConfig.parameters.type){
-                case 'string':
-                    stringParams = input(id: 'input', message: '', parameters:[
-                    [$class: 'StringParameterDefinition',defaultValue: '', description: '', 
-                    name: "${pipelineConfig.parameters.name}", trim: true]])
-                    break
-                case 'choice':
-                    def choices = pipelineConfig.parameters.choice.choices.replaceAll(',',"\n")            
-                    choiceParams = input(id: '', message: "${k}", parameters: [
-                    [$class: 'ChoiceParameterDefinition', choices: "${v}", name: "${k}"]])
-                    break
-                case 'password':
-                    PASSWORD = input(id: 'password', message: '', parameters: [
-                    [$class: 'PasswordParameterDefinition', name: "Password"]])
-                    break
-            }
-        }
+    def files = sh(returnStdout: true, script: "ls $workspace/runbook").replaceAll(".yml", "")
+    def yaml = input(id: 'tasklist', message: 'task', parameters: [
+        [$class: 'ChoiceParameterDefinition', choices: "${files}", description: '', name: 'tasklist']]) 
+        
+    builder = jobCfg("${yaml}")
+    
+    inputParams("${yaml}")
+
+    if(builder.notification){
+        def by = "${currentBuild.getBuildCauses()[0].userId}"
+        msTeamsNotif("Started ${by}", builder.project_name, builder.notification.webhook)
     }
-    stage("task: ${runbook}"){
-      def command = [:]
-      
-      if(pipelineConfig.notification){
-        common.sendTeamsNotif("${startedby}", pipelineConfig.project_name, pipelineConfig.notification.webhook)
-      }
- 
-      pipelineConfig.steps.each{step->
-        step.name.collect{name->
-          stage("${name}"){
-            pipelineConfig.steps.command.collect{v->
-                v.each{cmd->
-                    serversInParallel(server: "${server}", cmd: "${cmd}")
-                }        
+    stepBuilder = builder.steps
+    stepBuilder.each{->
+      stepBuilder.name.each{A->
+        stage("${A}"){
+          if(!stepBuilder.server){
+            stepBuilder.command.each{C->
+              echo 'local[SHELL]'
+                res = sh script: "${C}" }   
+                 } else{ stepBuilder.server.each{B->
+                    stepBuilder.command.each{C->
+sh """#!/bin/bash +x\nexport "\$(cat .env)";\n
+    ssh -F + ${B} "hostname; whoami;\n\
+export TERM=xterm-256color;
+    set -x; ${C}"
+"""                
             }
-         }
-      }
-   }
-}
-def serversInParallel(Map config){
-  def command = [:] 
-
-  if(!server || server == 'localhost'){
-    echo 'local[SHELL]'
-    res = config.cmd 
-    returnStdout: false
-    return res 
-  } 
-
-  def serverlist = config.server.toString().split(',')
-
-  if(serverlist.size() > 1){ 
-    for(i in serverlist){ 
-      def s = i.trim()
-        command[s] = { 
-        """#!/bin/bash +x\n\
-              export="\$(cat .env)"
-  ${sshArgs} ${server} "export TERM=xterm-256color;\n\
-  set -x; ${config.cmd}"
-        """ 
+          }
         }
-    }   
-    parallel command
+      }
+    }
   }
-  else { 
-    sh script: args + "${config.server}" + " " + config.cmd 
-  }
-}
-
-
-
-
-
-//
+}   
