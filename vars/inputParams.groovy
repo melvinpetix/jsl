e450a69bf650910db1298dfd538156ddbb48ac10
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-18 05:00:54 +0800

    Update inputParams.groovy

--- vars/inputParams.groovy
+++ vars/inputParams.groovy
@@ -14,11 +14,7 @@ def call(yaml){
         userInput = input(id: '', message: "${builder.parameters.choice.name}", 
         parameters: [[$class: 'ChoiceParameterDefinition', 
         choices: "${choices}", name: "${builder.parameters.choice.name}"]])  
-        sh """set +x; echo 'export ${builder.parameters.choice.name}=${userInput}"""
-        
-        writeFile file: "${builder.parameters.choice.name}.params", text: """
-        export ${builder.parameters.choice.name}=${userInput} >> .env"""
-        
+        sh """set +x; echo 'export ${builder.parameters.choice.name}=${userInput}"""        
     }
     if(builder.parameters.password){
         PASSWORD = input(id: 'password', message: '', parameters: [
