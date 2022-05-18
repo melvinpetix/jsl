eddc45e10e8b24dd4ac6eaada0cb9415a040f198
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-18 16:19:33 +0800

    Update inputParams.groovy

--- vars/inputParams.groovy
+++ vars/inputParams.groovy
@@ -6,21 +6,26 @@ def call(yaml){
         userInput = input(id: 'string', message: "${builder.parameters.string.name}", 
         parameters: [[$class: 'StringParameterDefinition', defaultValue: '', 
         description: "${builder.parameters.string.name}", name: '', trim: true]]) 
-        sh "set +x; echo \"${builder.parameters.string.name}=${userInput}\" >> .env"
+        if(userInput){
+           sh "set +x; echo \"${builder.parameters.string.name}=${userInput}\" >> .env"
+        }
     } 
-    if(builder.parameters.choice){       
+   if(builder.parameters.choice){       
         def choices = []
         choices = builder.parameters.choice.choices.toString().replaceAll(',','\n')
         userInput = input(id: '', message: "${builder.parameters.choice.name}", 
         parameters: [[$class: 'ChoiceParameterDefinition', 
         choices: "${choices}", name: "${builder.parameters.choice.name}"]])  
-        sh "set +x; echo \"${builder.parameters.choice.name}=${userInput}\" >> .env"        
-    }
+        if(userInput){
+            sh "set +x; echo \"${builder.parameters.choice.name}=${userInput}\" >> .env"        
+        }   
+    }    
     if(builder.parameters.password){
         PASSWORD = input(id: 'password', message: '', parameters: [
         [$class: 'PasswordParameterDefinition', name: "Password"]])
-        sh "set +x; echo \"${builder.parameters.choice.name}=${userInput}\" >> .env"
-        
+        if(PASSWORD){
+            sh "set +x; echo \"${builder.parameters.choice.name}=${PASSWORD}\" >> .env"
+        }
     }
   
 }
