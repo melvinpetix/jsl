8d6d73e6fe083652bc27769107ae1b899b6b678d
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-21 00:15:24 +0800

    Update inputParams.groovy

--- vars/inputParams.groovy
+++ vars/inputParams.groovy
@@ -1,25 +1,22 @@
 def call(yamlName){
     def choices
     j = jobCfg("$workspace/runbook/" + yamlName + ".yml")
+    
     if(j.parameters){
         if(j.parameters.string){
-                stringParams = input(id: 'userInput', message: "${j.parameters.string.name}", parameters: [[$class: 'StringParameterDefinition', 
-                defaultValue: '', description: "${j.parameters.string.name}", name: "${j.parameters.string.name}", trim: true]])
-                sh "set +x; echo \"${stringParams}\"=\"${j.parameters.string.name}\" >> .env"  
+            stringParams = input(id: 'userInput', message: "${j.parameters.string.name}", parameters: [[$class: 'StringParameterDefinition', 
+            defaultValue: '', description: "${j.parameters.string.name}", name: "${j.parameters.string.name}", trim: true]])
+            sh "set +x; echo \"${stringParams}\"=\"${j.parameters.string.name}\" >> .env"  
         }
-
         if(j.parameters.choice){
-            
-                j.parameters.choice.choices.collect{choice->
-                    choiceParams = input(id: '', message: "${j.parameters.choice.name}", 
-                    parameters: [[$class: 'ChoiceParameterDefinition', 
-                    choices: choice, description: '', 
-                    name: "${j.parameters.choice.name}"]])  
-                    sh "set +x; echo \"${choiceParams}\"=\"${j.parameters.string.name}\" >> .env"
-                    
-                }
+            choices = j.parameters.choice.choices.replaceAll(',',"\n")    
+            choiceParams = input(id: '', message: "${j.parameters.choice.name}", 
+            parameters: [[$class: 'ChoiceParameterDefinition', 
+            choices: choice, description: '', name: "${j.parameters.choice.name}"]])  
+            sh "set +x; echo \"${choiceParams}\"=\"${j.parameters.string.name}\" >> .env"        
         }
-    } else {
+    }
+     else {
         println 'no parameters settings for this job'
     }
 }
