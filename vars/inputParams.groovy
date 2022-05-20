d2d37ece9a05cfe9b706634f4ef60a141640a07d
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-20 19:40:23 +0800

    Update inputParams.groovy

--- vars/inputParams.groovy
+++ vars/inputParams.groovy
@@ -1,23 +1,21 @@
 def call(yamlName){
     j = jobCfg("$workspace/runbook/" + yamlName + ".yml")
-    
-    if(j.parameters.string){
-            stringParams = input(id: 'userInput', 
-            message: "${j.parameters.string.name}", 
-            parameters: [[$class: 'StringParameterDefinition', 
-            defaultValue: '', description: "${j.parameters.string.name}", 
-            name: "${j.parameters.string.name}", trim: true]])
-            sh "set +x; echo \"${stringParams}\"=\"${userInput}\" >> .env"  
-    }
+    if(j.parameters){
+        if(j.parameters.string){
+                stringParams = input(id: 'userInput', message: "${j.parameters.string.name}", parameters: [[$class: 'StringParameterDefinition', 
+                defaultValue: '', description: "${j.parameters.string.name}", name: "${j.parameters.string.name}", trim: true]])
+                sh "set +x; echo \"${stringParams}\"=\"${userInput}\" >> .env"  
+        }
 
-    if(j.parameters.choice){
-            def choices = j.parameters.choice.choices.replaceAll(',',"\n")
-            choiceParams = input(id: '', message: "${j.parameters.choice.name}", 
-            parameters: [[$class: 'ChoiceParameterDefinition', 
-            choices: "${choices}", description: '', 
-            name: "${j.parameters.choice.name}"]])  
-            sh "set +x; echo \"${choiceParams}\"=\"${userInput}\" >> .env"
+        if(j.parameters.choice){
+                def choices = j.parameters.choice.choices.replaceAll(',',"\n")
+                choiceParams = input(id: '', message: "${j.parameters.choice.name}", 
+                parameters: [[$class: 'ChoiceParameterDefinition', 
+                choices: "${choices}", description: '', 
+                name: "${j.parameters.choice.name}"]])  
+                sh "set +x; echo \"${choiceParams}\"=\"${userInput}\" >> .env"
 
+        }
     }
 }
 
