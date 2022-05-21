0dd83ac2a773d12cccd8e903ef73604a72e836f3
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-22 01:19:48 +0800

    Update buildSteps.groovy

--- vars/buildSteps.groovy
+++ vars/buildSteps.groovy
@@ -4,6 +4,7 @@ import com.webops.steps.Step;
 
 def call(ProjectConfiguration projectConfig) {
   return { ->
+    List<Step> stepsA = projectConfig.steps.steps
      stepsA.each { step ->
          stage(step.name) {
             step.commands.each { command ->
