69c1e1aa09511ca1d1ec56478d95b03f356ccc35
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-23 03:01:26 +0800

    Update inputParameter.groovy

--- vars/inputParameter.groovy
+++ vars/inputParameter.groovy
@@ -6,7 +6,7 @@ def call(yamlName){
             parameters: [[$class: 'StringParameterDefinition', 
             defaultValue: '', description: j.parameters.string.name, 
             name: j.parameters.string.name, trim: true]])
-            sh 'set +x; echo \"${j.parameters.string.name}=${params}\"'
+            sh "set +x; echo ${j.parameters.string.name}=${params}"
             //sh 'set +x; echo \"${j.parameters.string.name}=${params}\" >> config.sh'
             echo "${snapshot_date}"
         }
