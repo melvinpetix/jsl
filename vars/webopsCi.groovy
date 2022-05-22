6c7354f89ce92555ec31813e4989da24ca26504c
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-23 02:08:26 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -5,7 +5,7 @@ import com.webops.steps.Step;
 
 
 def call(String yamlName) {
-    def yaml = readYaml file: yamlName;
+    def yaml = readYaml file: "${workspace}/runbook/" + yamlName + ".yml";
     // load project's configuration
     ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
     // adds the last step of the build.
