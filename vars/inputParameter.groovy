4272ed6b4efa87c5bed7ea2e8d67b9422c61a702
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-23 02:37:59 +0800

    Update inputParameter.groovy

--- vars/inputParameter.groovy
+++ vars/inputParameter.groovy
@@ -2,11 +2,11 @@ def call(yamlName){
     j = jobCfg("$workspace/runbook/" + yamlName + ".yml")
     if(j.parameters){
         if(j.parameters.string){
-            j.parameter.string.name = input(id: 'userInput', message: '', 
+            j.parameters.string.name = input(id: 'userInput', message: '', 
             parameters: [[$class: 'StringParameterDefinition', 
             defaultValue: '', description: j.parameters.string.name, 
             name: j.parameters.string.name, trim: true]])
-            sh "set +x; echo \"${j.parameters.string.name}=${params}\" >> config.sh"
+            sh 'set +x; echo \'${j.parameters.string.name}=${params}\' >> config.sh'
         }
 
         if(j.parameters.choice){
@@ -15,7 +15,7 @@ def call(yamlName){
           parameters: [[$class: 'ChoiceParameterDefinition', 
           choices: "${choices}", description: '', 
           name: j.parameters.choice.name]])  
-          sh "set +x; echo \"${j.parameters.choice.name}=${params}\" >> config.sh"
+          sh 'set +x; echo \'${j.parameters.choice.name}=${params}\' >> config.sh'
         }
     } else {
         println 'no parameters settings for this job'
