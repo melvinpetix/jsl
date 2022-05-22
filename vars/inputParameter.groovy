708e623bc639260380a4f805fd5772a777f61b71
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-23 03:11:50 +0800

    Update inputParameter.groovy

--- vars/inputParameter.groovy
+++ vars/inputParameter.groovy
@@ -1,26 +1,22 @@
 def call(yamlName){
-    j = jobCfg(yamlName)
-    def ${j.parameters.string.name}
-    if(j.parameters){
-        if(j.parameters.string){
-            params = input(id: 'userInput', message: '', 
-            parameters: [[$class: 'StringParameterDefinition', 
-            defaultValue: '', description: j.parameters.string.name, 
-            name: j.parameters.string.name, trim: true]])
-            sh 'set +x; echo \"${j.parameters.string.name}=${params}\" >> config.sh'
-        }
-
-        if(j.parameters.choice){
-          def choices = j.parameters.choice.choices.replaceAll(',',"\n")
-          params = input(id: '', message: "${j.parameters.choice.name}", 
-          parameters: [[$class: 'ChoiceParameterDefinition', 
-          choices: "${choices}", description: '', 
-          name: j.parameters.choice.name]])  
-          sh 'set +x; echo \"${j.parameters.choice.name}=${params}\" >> config.sh'
-        }
-    } else {
-        println 'no parameters settings for this job'
+    j = jobCfg(yamlName)    
+    if(j.parameters.string){
+        params = input(id: 'userInput', message: '', 
+        parameters: [[$class: 'StringParameterDefinition', 
+        defaultValue: '', description: j.parameters.string.name, 
+        name: j.parameters.string.name, trim: true]])
+        sh 'set +x; echo \"${j.parameters.string.name}=${params}\" >> config.sh'
     }
+
+    if(j.parameters.choice){
+        def choices = j.parameters.choice.choices.replaceAll(',',"\n")
+        params = input(id: '', message: "${j.parameters.choice.name}", 
+        parameters: [[$class: 'ChoiceParameterDefinition', 
+        choices: "${choices}", description: '', 
+        name: j.parameters.choice.name]])  
+        sh 'set +x; echo \"${j.parameters.choice.name}=${params}\" >> config.sh'
+    }
+
 }
 
 
