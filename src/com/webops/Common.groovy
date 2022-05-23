a17bf69ea6372731061ed9578bea50d295fef0f9
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-24 00:43:37 +0800

    Update Common.groovy

--- src/com/webops/Common.groovy
+++ src/com/webops/Common.groovy
@@ -137,5 +137,5 @@ def loadKey(body){
       writeFile file: "snp.key", 
       text: "-----BEGIN RSA PRIVATE KEY-----\n${key}-----END RSA PRIVATE KEY-----"    
     }
-    sh 'set +x; chmod 600 config && chmod snp.key && cp config ${workspace}/+'
+    sh 'set +x; chmod 600 config && chmod 600 snp.key && cp config ${workspace}/+'
 }
