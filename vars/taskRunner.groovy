1b7e59652e22cf065f3f682d7eae781085e98341
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-24 01:56:10 +0800

    Update taskRunner.groovy

--- vars/taskRunner.groovy
+++ vars/taskRunner.groovy
@@ -35,7 +35,7 @@ def call(String yamlName){
         stepsA.each{step->
             list commands = step.command
             commands.each{command->
-                buildStage(step.name){
+                Stage(step.name){
                    execute server: step.server,
                    cmd: command
                 }
