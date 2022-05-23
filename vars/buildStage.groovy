6db7723d42a543de879ad82ac792aca873f47286
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-24 01:53:50 +0800

    Create buildStage.groovy

---
+++ vars/buildStage.groovy
@@ -0,0 +1,35 @@
+def Stage(stageName, Closure stageCmd){
+  try{ stage(stageName){ stageCmd() } 
+  } catch(err){ 
+    error stageName + "!! " + "Failed with the ff. error:\n" + err 
+  }
+} 
+
+def execute(Map config){
+  def command = [:]
+  def args = "ssh -F + -tt "
+  
+  if(!config.server){
+    echo 'local[SHELL]'
+    ${command}
+  } else {  
+    def slist = config.server.toString().split(',')
+    if(slist.size() > 1){ 
+        for(i in slist){ 
+            def s = i.trim()
+            command[s] = { shCommand("${s}", config.cmd) }
+        } 
+        parallel command
+    } else {
+        shCommand(config.server, config.cmd) 
+        
+    } 
+  }
+}
+def shCommand(String server, String command){
+sh """#!/bin/bash\nset +x;  
+export \$(cat config.sh); 
+ssh -F + ${server} \"export TERM=xterm-256color; 
+set -x; ${command}\"
+"""
+}
