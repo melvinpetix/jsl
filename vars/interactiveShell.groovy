630aa02f953bf189ffe5996902b77895124c133b
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-23 18:26:26 +0800

    Update interactiveShell.groovy

--- vars/interactiveShell.groovy
+++ vars/interactiveShell.groovy
@@ -1,15 +1,14 @@
 def call() {
+  stage 'shell session'
   timeout(time: 10, unit: 'MINUTES') {
     println "[Starting shell session]"
     String cmd = ''
     while (true) {
       cmd = input(id: 'cmd', message: 'Command:', parameters: [
       [$class: 'TextParameterDefinition',
-      description: 'shell session of the fly',
-      defaultValue: cmd, name: 'cmd']])
-      
-      if (cmd == 'exit') { break }
-      
+      description: '',
+      defaultValue: cmd, name: 'cmd']])    
+      if (cmd == 'exit') { break }    
       def ret = sh(returnStatus: true,
       script: cmd)
       echo "[Shell] Return Code: $ret"
