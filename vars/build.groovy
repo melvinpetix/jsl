c915e3155d60602c7bc43f55b75f789352e72b3c
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-26 03:43:19 +0800

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -40,24 +40,24 @@ def shCommand(String server, String command){
 }
 
 def params(yamlName){
-    def userInput
-    def j = readYaml file: 'runbook/' + yamlName + '.yml'
+  def userInput
+  def j = readYaml file: "runbook/${yamlName}.yml"
 
-    if(j.parameters.string){
-      userInput = input parameters: [string(defaultValue: '', 
-      description: j.parameters.string.description, 
-      name: j.parameters.string.name)]
-      env["${j.parameters.string.name}"] = userInput
-    }
-    if(j.parameters.choice){
-      def choices = j.parameters.choice.choices.replaceAll(',',"\n")
-      userInput = input parameters: [
-      choice(choices: choices ,name: j.parameters.choice.name)]
-      env["${j.parameters.choice.name}"] = userInput    
-    }
-    if(j.parameters.password){
-      P4SSWORD = input parameters: [password(name: 'P4SSWORD'] 
-      env["${j.parameters.password.name}"] = P4SSWORD
-    }
-     
-}
+  if(j.parameters.string){
+    userInput = input parameters: [string(defaultValue: '', 
+    description: j.parameters.string.description, 
+    name: j.parameters.string.name)]
+    env["${j.parameters.string.name}"] = userInput
+  }
+  if(j.parameters.choice){
+    def choices = j.parameters.choice.choices.replaceAll(',',"\n")
+    userInput = input parameters: [
+    choice(choices: choices ,name: j.parameters.choice.name)]
+    env["${j.parameters.choice.name}"] = userInput    
+  }
+  if(j.parameters.password){
+    P4SSWORD = input parameters: [
+    password(name: 'P4SSWORD')] 
+    env["${j.parameters.password.name}"] = P4SSWORD
+  }       
+}      
