d777e60d1061594e3b117b1493da58f42ea3ba20
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-26 03:51:36 +0800

    Update buildParams.groovy

--- vars/buildParams.groovy
+++ vars/buildParams.groovy
@@ -1,24 +1,23 @@
-def params(yamlName){
+def call(yamlName){
     def userInput
-    def j = readYaml file: 'runbook/' + yamlName + '.yml'
+    def j = readYaml file: "runbook/${yamlName}.yml"
 
     if(j.parameters.string){
-      userInput = input parameters: [string(defaultValue: '', 
-      description: j.parameters.string.description, 
-      name: j.parameters.string.name)]
-      env["${j.parameters.string.name}"] = userInput
+        userInput = input parameters: [string(defaultValue: '', 
+        description: j.parameters.string.description, 
+        name: j.parameters.string.name)]
+        env["${j.parameters.string.name}"] = userInput
     }
     if(j.parameters.choice){
-      def choices = j.parameters.choice.choices.replaceAll(',',"\n")
-      userInput = input parameters: [
-      choice(choices: choices ,name: j.parameters.choice.name)]
-      env["${j.parameters.choice.name}"] = userInput    
+        def choices = j.parameters.choice.choices.replaceAll(',',"\n")
+        userInput = input parameters: [
+        choice(choices: choices ,name: j.parameters.choice.name)]
+        env["${j.parameters.choice.name}"] = userInput    
     }
     if(j.parameters.password){
-      P4SSWORD = input parameters: [
-      password(name: 'PASSWORD')] 
-      env['PASSWORD'] = P4SSWORD
+         P4SSWORD = input parameters: [
+         password(name: 'P4SSWORD')] 
+         env["${j.parameters.password.name}"] = P4SSWORD
     }
-     
 }      
 
