211d30d61d3ede43a9bcd98e10fcf71ec54dab31
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-26 03:12:35 +0800

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -38,3 +38,27 @@ def shCommand(String server, String command){
   set -x; ${command}\"
   """
 }
+
+def params(yamlName){
+    def userInput
+    def j = readYaml file: 'runbook/' + yamlName + '.yml'
+
+    if(j.parameters.string){
+      userInput = input parameters: [string(defaultValue: '', 
+      description: j.parameters.string.description, 
+      name: j.parameters.string.name)]
+      env["${j.parameters.string.name}"] = userInput
+    }
+    if(j.parameters.choice){
+      def choices = j.parameters.choice.choices.replaceAll(',',"\n")
+      userInput = input parameters: [
+      choice(choices: choices ,name: j.parameters.choice.name)]
+      env["${j.parameters.choice.name}"] = userInput    
+    }
+    if(j.parameters.password){
+      P4SSWORD = input parameters: [
+      password(name: 'PASSWORD')] 
+      env['PASSWORD'] = P4SSWORD
+    }
+     
+}
