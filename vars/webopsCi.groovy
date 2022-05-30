c76c5c226c1c5f0085c2ffdc3d2c44c5e80c3fb7
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-05-30 17:29:26 +0800

    Update webopsCi.groovy

--- vars/webopsCi.groovy
+++ vars/webopsCi.groovy
@@ -9,13 +9,6 @@ def call(String yamlName) {
     // load project's configuration
     ProjectConfiguration projectConfig = ConfigParser.parse(yaml, env);
     // adds the last step of the build.
-    List<Step> stepsA = projectConfig.steps.step
-    stepsA.each { step ->
-     stage(step.name) {
-        step.commands.each { command ->
-           sh command
-        }
-      }
-    }
-    
+    def closure = buildSteps(projectConfig, customImage);
+    closure([:])
 }
