335545ed4f70c4d9ba3a7374f47d55ffefdc1f22
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-02 03:33:54 +0800

    Update Parser.groovy

--- src/com/webops/Parser.groovy
+++ src/com/webops/Parser.groovy
@@ -4,38 +4,22 @@ import com.webops.PipelineBuilder;
 
 class Parser {
 
-    static PipelineBuilder parse(def yaml, def env) {
+    static PipelineBuilder parse(def yaml) {
       
         PipelineBuilder pipelineBuilder = new PipelineBuilder();
 
-         // parse the environment variables and jenkins environment variables to be passed
-        pipelineBuilder.environment = parseEnvironment(yaml.environment, yaml.jenkinsEnvironment, env);
-
-        // parse the execution steps
         pipelineBuilder.steps = parseSteps(yaml.steps);
 
-        // load the project name
         pipelineBuilder.projectName = parseProjectName(yaml.project_name);
-
-        pipelineBuilder.env = env;
+        
+        pipelineBuilder.parameter = parseParameters(yaml.parameters);
+        
+        pipelineBuilder.notification = parseNotification(yaml.notification)
 
         return pipelineBuilder;
     }
 
-    def parseEnvironment(def environment, def jenkinsEnvironment, def env) {
-        def config = [];
 
-        if (environment) {
-            config += environment.collect { k, v -> "${k}=${v}"};
-        }
-
-        if (jenkinsEnvironment) {
-            config += jenkinsEnvironment.collect { k -> "${k}=${env.getProperty(k)}"};
-        }
-
-        return config;
-    }
-  
      def parseProjectName(def config) {
         if (!config || !config["project_name"]) {
             return "webops-generic-template";
