8b0ec0e5d773679eca372bfb1d24d26a3c2da6be
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-01 14:53:39 +0800

    Update taskRunner.groovy

--- vars/taskRunner.groovy
+++ vars/taskRunner.groovy
@@ -12,7 +12,7 @@ def call(yamlName){
         def yaml = readYaml file: 'runbook/' + yamlName + '.yml'
     
         if(yaml.parameters){
-            common.build('parameters',{this.&build.params(yamlName)})
+            common.build('parameters'){ build.params(yamlName)}
         }      
         
         if(yaml.environment){
