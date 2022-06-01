8d8dbc6ffdc91a2a1e932e0ff1df0cc11703549f
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-01 18:37:05 +0800

    Update Common.groovy

--- src/com/webops/Common.groovy
+++ src/com/webops/Common.groovy
@@ -81,7 +81,7 @@ def execute(Map config){
   }
 }
 
-def stage(String name, Closure stageCommands) {
+def buildStage(String name, Closure stageCommands) {
     try {
       stage(name, stageCommands)
     } catch(err) {
