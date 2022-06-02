d420efb1efb8809328b06556b68abe9ea1587f84
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-02 18:01:11 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -5,20 +5,16 @@ def call(String repo=null, String branch=null) {
   def yamlName
   node{
     deleteDir()
-    if(config.repo){
-      common.gitClone("${branch}", "${repo}")
-    } else {
-      checkout scm
-    }  
+    common.gitClone("${branch}", "${repo}")
     common.loadKey()
-    
-    stage 'task'
+    stage('task'){
       def folders = sh(returnStdout: true,  script: "ls $workspace/runbook").replaceAll(".yml", "")
       yamlName = input(id: 'tasklist', message: 'task', parameters: [
       [$class: 'ChoiceParameterDefinition', choices: "${folders}", description: '', name: '']]) 
+    }
   }
-    stage 'task2'
-      node("${env.jenkins_agent}"){
-        taskRunner(yamlName)
-      } 
+  stage 'task runner'
+  node("${env.jenkins_agent}"){
+      taskRunner(yamlName)
+  } 
 }
