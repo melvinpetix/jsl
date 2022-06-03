df8a6ae2b3d7d89594ffa8f05641d287f0e1951f
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-03 21:23:49 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,11 +1,11 @@
 import com.webops.*;
 
-def call(String repo=null, String branch=null) {
+def call(String repo, String branch) {
   def yamlName
   node("${env.jenkins_agent}"){
     deleteDir()  
     def common = new Common()
-    common.gitClone("${branch}", "${repo}")
+    common.gitClone("${repo}", "${branch}")
     stage('define task'){
       def folders = sh(returnStdout: true,  
       script: "ls $workspace/runbook").replaceAll(".yml", "")
