36fe047a0496364acbb1d4d1f406e441bddd4ffc
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-06 01:44:42 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,18 +1,18 @@
 @Library(value="github.com/melvinpetix/jsl@main", changelog=false)_
 
 
-def call(){
+def call(body){
 def node
 def runbook
+    node('maintenance-script-ec2-spot-worker'){
 
-node('maintenance-script-ec2-spot-worker'){
-    git url: 'https://oauth2:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git', branch: 'test'
-    def folders = sh(returnStdout: true, script: "ls $WORKSPACE/runbook").replaceAll(".yml", "")  
-    writeFile file: 'parameters', text: """\n\${folders}"""        
-    runbooks = readFile('parameters')
-    properties([parameters([choice(choices: "${runbook}", name: 'runbook')])])
-    yamlName = params.runbook
-    taskRunner yamlName
-  }
+        git url: 'https://oauth2:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git', branch: 'test'
+        def folders = sh(returnStdout: true, script: "ls $WORKSPACE/runbook").replaceAll(".yml", "")  
+        writeFile file: 'parameters', text: """\n\${folders}"""        
+        runbooks = readFile('parameters')
+        properties([parameters([choice(choices: "${runbook}", name: 'runbook')])])
+        yamlName = params.runbook
+        taskRunner yamlName
+    }
 }
-return this;  
+ 
