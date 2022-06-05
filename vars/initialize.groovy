6ffa6ee44d7203f1801dab611165289f4441b8dd
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-06 03:49:30 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,18 +1,18 @@
 @Library(value="github.com/melvinpetix/jsl@main", changelog=false)_
 
 
-def call(){
+def call(configPath='./runbook'){
  node {
-    def runbooks
+    def runbook
     git url: 'https://oauth2:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git', branch: 'test'
     def folders = sh(returnStdout: true, 
-    script: "ls $WORKSPACE/runbook").replaceAll(".yml", "")  
-    writeFile file: 'parameters', 
-    text: """
+    script: 'ls ' + configPath).replaceAll(".yml", "")  
+    writeFile file: 'runbook', 
+    text: """ 
 ${folders}
 """ 
     runbooks = readFile('parameters')
-    properties([parameters([choice(choices: "${runbooks}", name: 'runbook')])])
+    properties([parameters([choice(choices: "${runbook}", name: 'runbook')])])
     taskRunner params.runbook
  }  
 }
