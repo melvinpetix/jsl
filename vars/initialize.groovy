34e49130e06a02d452f9436721137cb96cb27e84
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-05 20:47:07 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -3,15 +3,9 @@ def call(String repo, String branch) {
   node("${env.jenkins_agent}"){
     deleteDir()  
     git branch: "${branch}", url: "${repo}"
-    stage('define runbooks'){
-      def folders = sh(returnStdout: true,  
-      script: "ls $workspace/runbook").replaceAll(".yml", "")
-      yamlName = input(message: 'runbook', parameters: [
-            [$class: 'ChoiceParameterDefinition', 
-            choices: "${folders}", 
-            description: '', name: '']])   
-    }  
-    
+    //stage('define runbooks'){
+    def folders = sh(returnStdout: true,  
+    script: "ls $workspace/runbook").replaceAll(".yml", "")      
     taskRunner(yamlName)
-  } 
+  }     
 }
