501ade071a15cca997f4b383b04b69001a585a19
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-06 03:55:42 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -6,7 +6,7 @@ def call(){
     def runbook
     git url: 'https://oauth2:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git', branch: 'test'
     def folders = sh(returnStdout: true, 
-    script: 'ls ./runbook').replaceAll(".yml", "")  
+    script: 'ls ./runbook/*').replaceAll(".yml", "")  
     writeFile file: 'runbook', 
     text: """ 
 ${folders}
