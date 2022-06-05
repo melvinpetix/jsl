409c02b431917eab17175a197ad0435da50384e8
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-06 02:54:21 +0800

    Update inputParams.groovy

--- vars/inputParams.groovy
+++ vars/inputParams.groovy
@@ -7,28 +7,4 @@ def call(params){
     }
 }
     
-    
-    /*
-    def userInput
-    def j = readYaml file: "runbook/${yamlName}.yml"
-
-    if(string){
-        userInput = input parameters: [string(defaultValue: '', 
-        description: j.parameters.string.description, 
-        name: j.parameters.string.name)]
-        env["${j.parameters.string.name}"] = userInput
-    }
-    if(j.parameters.choice){
-        def choices = j.parameters.choice.choices.toString().replaceAll(',',"\n")
-        userInput = input parameters: [
-        choice(choices: choices ,name: j.parameters.choice.name)]
-        env["${j.parameters.choice.name}"] = userInput    
-    }
-    if(j.parameters.password){
-         P4SSWORD = input parameters: [
-         password(name: 'P4SSWORD')] 
-         env["${j.parameters.password.name}"] = P4SSWORD
-    */
-    }
-    
-}      
+  
