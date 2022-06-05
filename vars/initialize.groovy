22505e9ef4826be38ef48552f29f8a16e2e5a4e7
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-06 01:32:33 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,13 +1,21 @@
 @Library(value="github.com/melvinpetix/jsl@main", changelog=false)_
 
-def call(String repo, String branch) {
-  def yamlName
-  node("${env.jenkins_agent}"){
-    deleteDir()  
-    git branch: "${branch}", url: "${repo}"
-    //stage('define runbooks'){
-    def folders = sh(returnStdout: true,  
-    script: "ls $workspace/runbook").replaceAll(".yml", "")      
-    taskRunner(yamlName)
-  }     
+
+def call(){
+def yamlName
+def runbook
+  node {
+    git url: 'https://oauth2:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git', branch: 'test'
+    def folders = sh(returnStdout: true, 
+    script: "ls $WORKSPACE/runbook").replaceAll(".yml", "")  
+    writeFile file: 'parameters', text: """\n\${folders}"""        
+    runbooks = readFile('parameters')
+    properties([parameters([choice(choices: "${runbook}", name: 'runbook')])])
+    yamlName = runbook
+    deleteDir()
+  }
+  node(${env.jenkins_agent}){
+    git url: 'https://oauth2:glpat-GxfR6J-STGecxjDPGz8z@gitlab.com/me1824/jsl.git', branch: 'test'
+    taskRunner yamlName
+  }
 }
