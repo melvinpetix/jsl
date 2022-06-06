e6864bc05ef751bdab3931a90502904ab65d76b8
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-06 15:26:05 +0800

    Update Common.groovy

--- src/com/webops/Common.groovy
+++ src/com/webops/Common.groovy
@@ -148,18 +148,15 @@ def inputParams(params){
     timeout(time: 120, unit: 'SECONDS') {  
       switch(params){
         case 'params.string':
-          userInput = input message: '', parameters: [
-              string(name: params.string.name)]
-              env[params.string.name] = userInput
+          userInput = input message: '', parameters: [string(name: params.string.name)]
+          env[params.string.name] = userInput
           break 
         case 'params.choice':
-          userInput = input message: '', parameters: [
-          choice(name: params.choice.name, choices: params.choice.choices)]
+          userInput = input message: '', parameters: [choice(name: params.choice.name, choices: params.choice.choices)]
           env[params.choice.name] = userInput
         case 'params.password':
-          userInput = input parameters: [
-              password(name: '')]
-              env[params.password.name] = userInput
+          userInput = input parameters: [password(name: '')]
+          env[params.password.name] = userInput
 
         }
     }
