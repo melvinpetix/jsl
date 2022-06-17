722ca19dc8e73596b2e05d4aa08bbe21ee516fa6
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-18 02:46:42 +0800

    Update taskRunner.groovy

--- vars/taskRunner.groovy
+++ vars/taskRunner.groovy
@@ -21,6 +21,7 @@ def call(yamlName){
   try{   
     if(yaml.parameters){          
       def myProps = readMyProps yaml.parameters
+        
       timeout(time: 120, unit: 'SECONDS') {
         input parameters: myProps         
       }      
@@ -66,3 +67,16 @@ def call(yamlName){
 
   deleteDir()  
 }
+
+@NonCPS
+def readMyProps(parameters) {
+    parameters.collect { params ->
+      this.invokeMethod params.type, params.args.collectEntries { name, value ->
+        [
+          name, 
+          value instanceof String ? interp(value) : value
+        ]
+      }
+    }
+ }
+
