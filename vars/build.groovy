479f12c6d9684a5b76eb8015ead96976e946eb54
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-19 04:44:28 +0800

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -6,23 +6,23 @@ def call(yamlName){
 }
 
 def params(){
-  params = yaml.parameters
   stage('build parameters'){
-    timeout(time: 120, unit: 'SECONDS') {  
-      switch(params.type){
-        case 'string':
-        userInput = input message: '', parameters: [string(name: params.args.name)]
-        break    
-        case 'choice':
-        userInput = input message: '', parameters: [choice(name: params.args.name, choices: params.args.choices)]; 
-        break
-        case 'password':
-        userInput = input parameters: [password(name: password)]; 
-        break           
-     } 
-      return env."${params.args.name}" = userInput   
-    }
-  }
+     yaml.parameters.each{params->
+      timeout(time: 120, unit: 'SECONDS') {  
+         switch(params.type){
+            case 'string':
+            userInput = input message: '', parameters: [string(name: params.args.name)]
+            break    
+            case 'choice':
+            userInput = input message: '', parameters: [choice(name: params.args.name, choices: params.args.choices)]; 
+            break
+            case 'password':
+            userInput = input parameters: [password(name: password)]; 
+            break           
+         }    
+      }
+      return env."${params.args.name}" = userInput    
+   }
 }
 
 def notification(String buildStatus, String jobName, webhookUrl) {     
