ace6a791c140090ee2195b65caf24d050b1b5f7c
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-19 07:29:00 +0800

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,42 +1,28 @@
 #!groovy
 import com.webops.*
 
-def call(body){
+def call(yaml){
    def common = new Common()
-   def config = body
-   def yaml = readYaml file: 'runbook/' + config.yaml + '.yml'
+   def yaml = readYaml file: yaml
   
    if(yaml.parameters){
-      def paramsConfig = parseParams yaml.parameters
       stage('pipeline parameters'){
         timeout(time: 120, unit: 'SECONDS') {  
-          input parameters: paramsConfig
-             
-        }
-      }
-   }
-             
-             /*
-             
-             
+           yaml.parameters.each{params->
             switch(params.type){
                case 'string':
-               userInput = input message: '', parameters: [string(name: params.args.name)]
-               break    
+                 input message: '', parameters: [string(name: params.args.name)]; break    
                case 'choice':
-               userInput = input message: '', parameters: [choice(name: params.args.name, choices: params.args.choices)]; 
-               break
+                 input message: '', parameters: [choice(name: params.args.name, choices: params.args.choices)]; break
                case 'password':
-               userInput = input parameters: [password(name: params.args.name)]; 
-               break           
+                 input parameters: [password(name: params.args.name)]; break           
              }  
-               return env."${params.args.name}" = userInput
+             return env."${params.args.name}" = params.args.name
            }     
          }
       }
    }
    
-   */
    if(yaml.notification){    
       def userName = "${currentBuild.getBuildCauses()[0].userId}"
       common.sendTeamsNotif(
@@ -45,25 +31,5 @@ def call(body){
         url: "${yaml.notification.webhook}"
       )
     } 
-}
-
-@NonCPS
-def parseParams(parameters) {
-    parameters.collect { params ->
-      this.invokeMethod params.type, params.args.collectEntries { name, value ->
-        [
-          name, 
-          value.toString()
-        ]
-      }
-    }
-}
-
-@NonCPS
-def interp(value) {
-  new groovy.text.GStringTemplateEngine()
-    .createTemplate(value)
-    .make([env:env])
-    .toString()
 }
 
