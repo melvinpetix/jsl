77c709361356438064b06554f500c70f89325e8d
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-19 05:12:17 +0800

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,25 +1,30 @@
 #!groovy
 
-def call(yamlName){
+def call(body){
+   def config = body
+   def yamlName = config.yaml
    def yaml = readYaml file: 'runbook/' + yamlName + '.yml'
-}
-def params(){
-  stage('build parameters'){
-     yaml.parameters.each{params->
-      timeout(time: 120, unit: 'SECONDS') {  
-         switch(params.type){
-            case 'string':
-            userInput = input message: '', parameters: [string(name: params.args.name)]
-            break    
-            case 'choice':
-            userInput = input message: '', parameters: [choice(name: params.args.name, choices: params.args.choices)]; 
-            break
-            case 'password':
-            userInput = input parameters: [password(name: password)]; 
-            break           
-          }  
-         return env."${params.args.name}" = userInput
-        }     
+   //def params = yaml.parameters
+   //def notification = yaml.notification
+   stage('define pipeline config'){
+      def userInput
+      if(yaml.parameters){     
+        timeout(time: 120, unit: 'SECONDS') {  
+          yaml.parameters.each{params->   
+            switch(params.type){
+               case 'string':
+               userInput = input message: '', parameters: [string(name: params.args.name)]
+               break    
+               case 'choice':
+               userInput = input message: '', parameters: [choice(name: params.args.name, choices: params.args.choices)]; 
+               break
+               case 'password':
+               userInput = input parameters: [password(name: password)]; 
+               break           
+             }  
+               return env."${params.args.name}" = userInput
+           }     
+         }
       }
    }
 }
