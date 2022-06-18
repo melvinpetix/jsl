2bdd65c8a307cc44e5686c25b4a89990da82171a
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-19 01:32:18 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -1,14 +1,15 @@
 @Library(value="github.com/melvinpetix/jsl@main", changelog=false)_
 
 def call(){
- 
- node {    
-  checkout scm  
-  if(!params.runbook || params.runbook == 'null'){ 
-    def folders = sh(returnStdout: true, 
-    script: "ls $WORKSPACE/runbook").replaceAll(".yml", "")  
-    String jp = """\n${folders}\n""" 
-    properties([parameters([choice(choices: "${jp}", name: 'runbook')])])   
-  }   
- taskRunner params.runbook   
+   if(!params.runbook || params.runbook == 'null'){ 
+   node {    
+      checkout scm  
+      def folders = sh(returnStdout: true, 
+      script: "ls $WORKSPACE/runbook").replaceAll(".yml", "")  
+      String jp = """\n${folders}\n""" 
+      properties([parameters([choice(choices: "${jp}", name: 'runbook')])])   
+   } else {
+    node(jenkins_agent){  
+     taskRunner params.runbook   
+  }
 }
