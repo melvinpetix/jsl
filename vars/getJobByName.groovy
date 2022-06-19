c9b92d8dfd43099c96e8f44e242f411efda80a3c
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-20 02:39:27 +0800

    Delete getJobByName.groovy

--- vars/getJobByName.groovy
+++
@@ -1,7 +0,0 @@
-def call(jobName, regexp=false){
-    for(item in Hudson.instance.items) {
-        if (regexp && item.name ==~ jobName || item.name == jobName) {
-            return item
-        }
-    }
-}
