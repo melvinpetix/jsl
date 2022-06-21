2aa3b40ee8ad7b2c965dd32721aa207d68c6affe
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-22 00:50:39 +0800

    Update Common.groovy

--- src/com/webops/Common.groovy
+++ src/com/webops/Common.groovy
@@ -10,7 +10,7 @@ import org.yaml.snakeyaml.DumperOptions
 
 
 
-def parseParameters(def yaml){
+def parseParameters(parameters){
   def userInput
   if(yaml.parameters){ 
     def inputPrompt = parseParams yaml.parameters
