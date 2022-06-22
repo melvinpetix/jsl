1e5d750d17aa695a737dd933dd5028db7a930742
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-22 23:12:46 +0800

    Update initialize.groovy

--- vars/initialize.groovy
+++ vars/initialize.groovy
@@ -2,10 +2,9 @@ import com.webops.*;
 
 def call(){
   node(jenkins_agent){        
+  
     checkout scm
-    def common = new Common() 
-    .loadKey() 
-    stage(params.runbook){
+
        taskRunner params.runbook 
     }
   }
