9aa3a4d196832bb8d2cfc22731334c5e04b048c5
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-22 22:40:29 +0800

    Update taskRunner.groovy

--- vars/taskRunner.groovy
+++ vars/taskRunner.groovy
@@ -1,6 +1,5 @@
 import com.webops.*;
 
-
 def call(yamlName){
   
   def common = new Common()
@@ -14,29 +13,18 @@ def call(yamlName){
     }
   } 
   
-  def userInput
-  
   def yaml = readYaml file: 'runbook/' + yamlName + '.yml'
-
+  
   try{ 
 
     if(yaml.parameters){
-      common.stage('build parameters'){
-        def inputPrompt = common.parseParams yaml.parameters
-        timeout(time: 120, unit: 'SECONDS') {
-          userInput = input parameters: inputPrompt     
-        } 
-        userInput.each{x,v-> env."$x"="$v"}      
-      }
-    }
-    
-    if(yaml.notification){
-      def userName = "${currentBuild.getBuildCauses()[0].userId}"
-      common.sendTeamsNotif(
-        msg: "Started by: ${userName}", 
-        job: "${yaml.project_name}", 
-        url: "${yaml.notification.webhook}"
-      )
+      def inputPrompt = parseParams yaml.parameters
+      timeout(time: 120, unit: 'SECONDS') {
+        yaml.parameters.each{params->
+          params.args.name = input parameters: inputPrompt 
+          println params.args.name    
+        }      
+      }             
     }
     
     if(!yaml.steps){
@@ -66,7 +54,26 @@ def call(yamlName){
 
   deleteDir()  
 }
-       
+
+@NonCPS
+def parseParams(parameters) {
+  parameters.collect { params ->
+    this.invokeMethod params.type, 
+    params.args.collectEntries { name, value ->
+      [
+        name, value instanceof String ? exportEnv(value) : value
+      ]
+    }
+  }
+}
+
+@NonCPS
+def exportEnv(value) {
+  new groovy.text.GStringTemplateEngine()
+    .createTemplate(value)
+    .make([env:env])
+    .toString()
+}        
      
     /*
     if(yaml.parameters){          
