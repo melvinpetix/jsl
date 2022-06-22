1cb4dea182cbcfd5fe17fcbd0c6d02a7fcdfad74
melvinpetix <66393736+melvinpetix@users.noreply.github.com>
2022-06-22 22:58:23 +0800

    Update taskRunner.groovy

--- vars/taskRunner.groovy
+++ vars/taskRunner.groovy
@@ -1,8 +1,11 @@
 import com.webops.*;
 
-def call(yamlName){  
+
+def call(yamlName){
   
   def yaml = readYaml file: 'runbook/' + yamlName + '.yml'
+  
+  def common = new Common()
   
   if(!yamlName || yamlName == 'null'){ 
     try { groovyShell() } catch(err){ 
@@ -15,13 +18,21 @@ def call(yamlName){
   try{ 
 
     if(yaml.parameters){
-      common.stage 'build pipeline'
-      def inputPrompt = parseParams yaml.parameters
-      timeout(time: 120, unit: 'SECONDS') {
-        yaml.parameters.each{params->
-          params.args.name = input parameters: inputPrompt 
-        }      
-      }             
+     def userInput    
+     def inputPrompt = common.parseParams yaml.parameters
+     timeout(time: 120, unit: 'SECONDS') {
+       userInput = input parameters: inputPrompt     
+     } 
+      userInput.each{x,v-> env."$x"="$v"}      
+   }
+    
+    if(yaml.notification){
+      def userName = "${currentBuild.getBuildCauses()[0].userId}"
+      common.sendTeamsNotif(
+        msg: "Started by: ${userName}", 
+        job: "${yaml.project_name}", 
+        url: "${yaml.notification.webhook}"
+      )
     }
     
     if(!yaml.steps){
@@ -51,54 +62,4 @@ def call(yamlName){
 
   deleteDir()  
 }
-
-@NonCPS
-def parseParams(parameters) {
-  parameters.collect { params ->
-    this.invokeMethod params.type, 
-    params.args.collectEntries { name, value ->
-      [
-        name, value instanceof String ? exportEnv(value) : value
-      ]
-    }
-  }
-}
-
-@NonCPS
-def exportEnv(value) {
-  new groovy.text.GStringTemplateEngine()
-    .createTemplate(value)
-    .make([env:env])
-    .toString()
-}        
-     
-    /*
-    if(yaml.parameters){          
-      def myProps = common.parseParams yaml.parameters
-      timeout(time: 120, unit: 'SECONDS') {
-        input parameters: myProps         
-      }      
-    } 
-     
-    if(yaml.parameters){
-       common.stage("${yamlName} parameters"){
-           yaml.parameters.each{params->
-               common.inputParams(params)
-            }
-       }    
-    }
-    
-    if(yaml.environment){
-      yaml.environment.each{env->
-        env.collect{k,v-> env."${k}"="${v}"}
-      }
-    }
-    if(yaml.notification){
-      def userName = "${currentBuild.getBuildCauses()[0].userId}"
-      common.sendTeamsNotif(
-        msg: "Started by: ${userName}", 
-        job: "${yaml.project_name}", 
-        url: "${yaml.notification.webhook}"
-      )
-    }
-   */
+   
